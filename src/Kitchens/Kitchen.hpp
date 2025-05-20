9957155790af736bafbc6928cb26d9df662ca8aa
Valiance <lucas.ludovic@epitech.eu>
2025-05-20 14:59:05 +0200

    feat(IPC): add interface and abstract for network communication

---
+++ src/Kitchens/Kitchen.hpp
@@ -0,0 +1,20 @@
+//
+// EPITECH PROJECT, 2025
+// Kitchen
+// File description:
+// Kitchen
+//
+
+#pragma once
+
+namespace plazza {
+    class Kitchen {
+       public:
+        Kitchen();
+        ~Kitchen() = default;
+
+        void run();
+
+       private:
+    };
+}  // namespace plazza
---
+++ src/Networks/ANetwork.cpp
@@ -0,0 +1,17 @@
+//
+// EPITECH PROJECT, 2025
+// ANetwork
+// File description:
+// Implementation for ANetwork functions
+//
+
+#include "ANetwork.hpp"
+#include <unistd.h>
+
+void Network::ANetwork::close()
+{
+    if (this->_fd > 0) {
+        ::close(this->_fd);
+    }
+    this->_fd = -1;
+}
