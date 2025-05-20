eb2d85aaf709739c24c757ec550a70c0fe57e558
Oursegamin <axel.lavrador@gmail.com>
2025-05-20 17:19:23 +0200

    feat: Renderers

---
+++ src/Renderer/SFMLRenderer.hpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2024
+** Plazza
+** File description:
+** SFMLRenderer.hpp
+*/
+
+#pragma once
+
+#include "IRenderer.hpp"
+
+#include <SFML/Graphics.hpp>
+
+namespace plazza {
+    class SFMLRenderer : public IRenderer
+    {
+        public:
+            SFMLRenderer() : _takeOrder(false) {}
+            ~SFMLRenderer() = default;
+
+            void init() override;
+
+            bool shouldClose() override { return _window.isOpen(); }
+
+            void update() override;
+
+            void render() override;
+
+            bool shouldTakeOrder() override { return _takeOrder; }
+            std::string takeOrder() override;
+
+            void showError(const std::string &message);
+
+        private:
+            bool _takeOrder;
+            std::string _order;
+
+            sf::RenderWindow _window;
+    };
+}
