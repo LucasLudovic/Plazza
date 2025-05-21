fee2c2ca628d84ad6d45541be5b963a11fdc499a
Oursegamin <axel.lavrador@gmail.com>
2025-05-21 14:53:20 +0200

    feat: attribution of pizzas

--- src/Data.cpp
+++ src/Data.cpp
@@ -17,16 +17,7 @@ plazza::PizzaType plazza::convertPizzaType(const std::string &str)
         if (pizzaType == pizzaTypeString[i])
             return static_cast<plazza::PizzaType>(std::pow(2, i));
     }
-    throw OrderError("Invalid pizza type", "Plazza");
-}
-
-std::string plazza::convertPizzaType(plazza::PizzaType pizzaType)
-{
-    for (size_t i = 0; i < pizzaTypeString.size(); i++) {
-        if (pizzaType == static_cast<plazza::PizzaType>(std::pow(2, i)))
-            return pizzaTypeString[i];
-    }
-    throw OrderError("Invalid pizza type", "Plazza");
+    throw plazza::OrderError("Invalid pizza type", "Plazza");
 }
 
 plazza::PizzaSize plazza::convertPizzaSize(const std::string &str)
@@ -35,23 +26,5 @@ plazza::PizzaSize plazza::convertPizzaSize(const std::string &str)
         if (str == pizzaSizeString[i])
             return static_cast<plazza::PizzaSize>(std::pow(2, i));
     }
-    throw OrderError("Invalid pizza size", "Plazza");
-}
-
-std::string plazza::convertPizzaSize(plazza::PizzaSize pizzaSize)
-{
-    for (size_t i = 0; i < pizzaSizeString.size(); i++) {
-        if (pizzaSize == static_cast<plazza::PizzaSize>(std::pow(2, i)))
-            return pizzaSizeString[i];
-    }
-    throw OrderError("Invalid pizza size", "Plazza");
-}
-
-unsigned int plazza::unPow(unsigned int nb, unsigned int pow)
-{
-    for (unsigned int i = 0; i < nb; i++) {
-        if (std::pow(pow, i) == nb)
-            return i;
-    }
-    throw OrderError("Invalid pizza size", "Plazza");
+    throw plazza::OrderError("Invalid pizza size", "Plazza");
 }
