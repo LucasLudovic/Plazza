9163d56b060c7fd621c5803f1269b89bee0a540a
Valiance <lucas.ludovic@epitech.eu>
2025-05-22 11:52:11 +0200

    feat(kitchen): store parameters in kitchen

--- src/Kitchens/Base.cpp
+++ src/Kitchens/Base.cpp
@@ -7,9 +7,14 @@
 
 #include "Kitchens/Kitchen.hpp"
 #include "Network/Server/Server.hpp"
-plazza::Kitchen::Kitchen(Network::ClientInfo_t &client_info)
+
+plazza::Kitchen::Kitchen(Network::ClientInfo_t &client_info, int cookingMultiplier,
+            int nbCooks, int restockTime)
 {
     this->_fd = client_info.clientFd;
+    this->_cookingTimeMultiplier = cookingMultiplier;
+    this->_nbCooks = nbCooks;
+    this->_restockTime = restockTime;
 }
 
 void plazza::Kitchen::run()
